82fdae296039bc4ed1f7c6ceee91c74cab98b7fd
Valian <jakub.skalecki@gmail.com>
2016-01-25 00:15:06 +0100

    removed unnecessary function

--- test.py
+++ test.py
@@ -1,28 +1,7 @@
 from __future__ import with_statement
-import sys
-import os.path
 from data import DataReader, CrossValidation
 from decision_tree import DecisionTreeCreator, RandomForestCreator
 from id3 import gain
-
-
-def get_file_names():
-    if len(sys.argv) < 2:
-        data_file_name = raw_input("Data Filename: ")
-    else:
-        data_file_name = sys.argv[1]
-
-    def file_exists(filename):
-        if os.path.isfile(filename):
-            return True
-        else:
-            print "Error: The file '%s' does not exist." % filename
-            return False
-
-    if not file_exists(data_file_name):
-        sys.exit(0)
-
-    return data_filename
 
 
 if __name__ == "__main__":
