aa99182f66318ea831ad12ab47e91df558f86f4c
Valian <jakub.skalecki@gmail.com>
2016-01-25 02:40:57 +0100

    Data related methods moved to DataSet class

--- core.py
+++ core.py
@@ -1,27 +1,30 @@
 import collections
-import random
-
-
-def sample_with_replacement(data, sample_size, replacement=True):
-    if replacement:
-        return [random.choice(data) for _ in xrange(sample_size)]
-    else:
-        return random.sample(data, sample_size)
 
 
 class RandomForestCreator(object):
 
     def __init__(self, tree_creator, num_of_trees=10, with_replacement=True, sample_size=0.6):
+        """
+        :type tree_creator: DecisionTreeCreator
+        :type num_of_trees: int
+        :type with_replacement: bool
+        :type sample_size: float
+        """
         self.num_of_trees = num_of_trees
         self.sample_size = sample_size
         self.with_replacement = with_replacement
         self.tree_creator = tree_creator
 
-    def create_random_forest(self, data, attributes, target_attr):
+    def create_random_forest(self, data):
+        """
+        :type data: data.DataSet
+        :return: RandomForest
+        """
         forest = RandomForest()
+        sample_size = int(len(data) * self.sample_size + 1)
         for i in xrange(self.num_of_trees):
-            sample_data = sample_with_replacement(data, int(len(data) * self.sample_size + 1), self.with_replacement)
-            tree = self.tree_creator.create_decision_tree(sample_data, attributes, target_attr)
+            sample_data = data.get_random_samples(sample_size, self.with_replacement)
+            tree = self.tree_creator.create_decision_tree(sample_data)
             forest.add_tree(tree)
         return forest
 
@@ -32,9 +35,16 @@ class RandomForest(object):
         self.trees = []
 
     def add_tree(self, tree):
+        """
+        :type tree: DecisionTreeNode
+        """
         self.trees.append(tree)
 
     def classify(self, record):
+        """
+        :type record: dict[str:str]
+        :rtype: str
+        """
         results = collections.defaultdict(lambda: 0)
         for tree in self.trees:
             single_result = tree.classify(record)
@@ -55,36 +65,28 @@ class DecisionTreeCreator(object):
         self.max_depth = max_depth
         self.fitness_func = fitness_func
 
-    def create_decision_tree(self, data, attributes, target_attr, actual_depth=0):
+    def create_decision_tree(self, data, _actual_depth=0):
         """
         Returns a new decision tree based on the examples given.
-        :type data: list[dict[str:str]]
-        :type attributes: list[str]
-        :type target_attr: str
-        :type actual_depth: int
+        :type data: data.DataSet
+        :type _actual_depth: int
         """
-        all_values_for_attribute = [record[target_attr] for record in data]
-        most_common_in_children = max(set(all_values_for_attribute), key=all_values_for_attribute.count)
-        reached_max_depth = self.max_depth is not None and actual_depth >= self.max_depth
-        if not data or (len(attributes) - 1) <= 0 or reached_max_depth:
-            return DecisionTreeNode(target_attr, value=most_common_in_children)
-        elif len(set(all_values_for_attribute)) == 1:
-            return DecisionTreeNode(target_attr, value=all_values_for_attribute[0])
+        most_common_class = data.get_most_common_class()
+        reached_max_depth = self.max_depth is not None and _actual_depth >= self.max_depth
+        if not data or (len(data.attributes) - 1) <= 0 or reached_max_depth:
+            return DecisionTreeNode(data.target_attr, value=most_common_class)
+        elif len(data.get_unique_values_of_attribute(data.target_attr)) == 1:
+            return DecisionTreeNode(data.target_attr, value=data.data[0][data.target_attr])
         else:
-            best = max(attributes, key=lambda a: self._calculate_fitness(data, a, target_attr))
-            tree = DecisionTreeNode(best, most_common_in_children)
-            unique_values = set(record[best] for record in data)
+            best = data.get_best_attribute(self.fitness_func)
+            tree = DecisionTreeNode(best, most_common_class)
+            unique_values = data.get_unique_values_of_attribute(best)
             for val in unique_values:
-                examples = filter(lambda record: record[best] == val, data)
-                attributes = [attr for attr in attributes if attr != best]
-                subtree = self.create_decision_tree(examples, attributes, target_attr, actual_depth + 1)
+                examples = data.get_all_with_given_attribute(best, val)
+                subtree = self.create_decision_tree(examples, _actual_depth + 1)
                 tree.add_child(subtree, val)
         return tree
 
-    def _calculate_fitness(self, data, attr, target_attr):
-        if attr == target_attr:
-            return 0.0
-        return self.fitness_func(data, attr, target_attr)
 
 
 class DecisionTreeNode(object):
@@ -96,6 +98,10 @@ class DecisionTreeNode(object):
         self._children = {}
 
     def add_child(self, node, attr_value):
+        """
+        :type node: DecisionTreeNode
+        :type attr_value: Any
+        """
         assert node.value not in self._children, "Already in subtree"
         self._children[attr_value] = node
 
@@ -103,6 +109,10 @@ class DecisionTreeNode(object):
         return len(self._children) == 0
 
     def classify(self, record):
+        """
+        :type record: dict[str:str]
+        :rtype: str
+        """
         if self.is_leaf():
             return self.value
         attr_value = record.get(self.attr_name)
@@ -118,7 +128,8 @@ class DecisionTreeNode(object):
     def __str__(self):
         if self.is_leaf():
             return str(self.value)
-        children = ['--> {0}:\n\t{1}'.format(val, str(child).replace('\n', '\n\t')) for val, child in self._children.iteritems()]
+        children = ['--> {0}:\n\t{1}'.format(val, str(child).replace('\n', '\n\t'))
+                    for val, child in self._children.iteritems()]
         return 'Attr: {0}\n{1}'.format(self.attr_name, '\n'.join(children))
 
 
@@ -128,31 +139,35 @@ class CrossValidation(object):
         self.folds = folds
         self.verbosity_level = verbosity_level
 
-    def train(self, training_set, attributes, target_attr):
-        forest = self.random_forest_creator.create_random_forest(training_set, attributes, target_attr)
+    def validate(self, data_set):
+        """
+        :param data_set: Data to perform cross validation
+        :type data_set: data.DataSet
+        :return: Result of using cross validation on given set
+        :rtype: float
+        """
+        subset_size = len(data_set.data) / self.folds
+        results = []
+        for i in range(self.folds):
+            test_set, train_set = data_set.split(i * subset_size, (i + 1) * subset_size)
+            forest = self._train(train_set)
+            results.append(self._test(test_set, forest))
+            if self.verbosity_level >= 1:
+                print 'Accuracy for fold no {0}: {1}'.format(i + 1, results[-1])
+        return sum(results) / self.folds
+
+    def _train(self, training_set):
+        forest = self.random_forest_creator.create_random_forest(training_set)
         if self.verbosity_level >= 3:
             print forest
         return forest
 
-    def test(self, test_set, target_attr, forest):
-        classification = forest.classify_many(test_set)
+    def _test(self, test_set, forest):
+        classification = forest.classify_many(test_set.data)
         valid_counter = 0
         for i, res in enumerate(classification):
-            expected = test_set[i][target_attr]
+            expected = test_set.data[i][test_set.target_attr]
             valid_counter += int(expected == res)
             if self.verbosity_level >= 2:
                 print 'result: {0:10}, expected {1:10} - {2}'.format(res, expected, expected == res)
         return valid_counter / float(len(test_set))
-
-    def validate(self, data_set):
-        subset_size = len(data_set.data) / self.folds
-        results = []
-        for i in range(self.folds):
-            testing_this_round = data_set.data[i * subset_size:(i + 1) * subset_size]
-            training_this_round = data_set.data[:i * subset_size] + data_set.data[(i + 1) * subset_size:]
-            forest = self.train(training_this_round, data_set.attributes, data_set.target_attr)
-            results.append(self.test(testing_this_round, data_set.target_attr, forest))
-            if self.verbosity_level >= 1:
-                print 'Accuracy for fold no {0}: {1}'.format(i + 1, results[-1])
-
-        return sum(results) / self.folds
--- data.py
+++ data.py
@@ -1,4 +1,6 @@
 import csv
+import random
+import collections
 
 
 class DataReader(object):
@@ -25,7 +27,75 @@ class DataReader(object):
 
 
 class DataSet(object):
+
     def __init__(self, attributes, data):
         self.attributes = attributes
         self.data = data
         self.target_attr = attributes[-1]
+
+    def split(self, from_index, to_index):
+        """
+        :type from_index: int
+        :type to_index: int
+        :rtype: DataSet
+        """
+        inner_data = self.data[from_index:to_index]
+        outer_data = self.data[:from_index] + self.data[to_index:]
+        return DataSet(self.attributes, inner_data), DataSet(self.attributes, outer_data)
+
+    def get_random_samples(self, amount, with_replacement=True):
+        """
+        :type amount: int
+        :type with_replacement: bool
+        :rtype: DataSet
+        """
+        if with_replacement:
+            data = [random.choice(self.data) for _ in xrange(amount)]
+        else:
+            data = random.sample(self.data, amount)
+        return DataSet(self.attributes, data)
+
+    def get_all_with_given_attribute(self, attribute, value):
+        """
+        :type attribute: string
+        :type value: string
+        :rtype: DataSet
+        """
+        data = filter(lambda record: record[attribute] == value, self.data)
+        attributes = [attr for attr in self.attributes if attr != attribute]
+        return DataSet(attributes, data)
+
+    def get_unique_values_of_attribute(self, attribute):
+        """
+        :type attribute: string
+        :return: set
+        """
+        return set(record[attribute] for record in self.data)
+
+    def get_values_frequency(self, attribute):
+        """
+        :return: dict[str:str]
+        """
+        counter = collections.defaultdict(lambda: 0)
+        for record in self.data:
+            counter[record[attribute]] += 1
+        return counter
+
+    def get_most_common_class(self):
+        """
+        :return: str
+        """
+        counter = self.get_values_frequency(self.target_attr)
+        return max(counter.iterkeys(), key=lambda k: counter[k])
+
+    def get_best_attribute(self, fitness_func):
+        """
+        :param fitness_func: (DataSet, str) => float
+        :return: str
+        """
+        return max(
+            self.attributes, key=lambda attr:
+            0.0 if attr == self.target_attr else fitness_func(self, attr))
+
+    def __len__(self):
+        return len(self.data)
